66efa416480201dc89d4e64d0a43c2549f085be4
Eduardo-Hinojosa <fsfrost@17gmail.com>
2022-11-11 11:32:20 -0300

    Use env file as JSON.

--- main.go
+++ main.go
@@ -1,98 +1,58 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
-	"regexp"
 )
 
 func main() {
-	pPath := flag.String("path", ".env", "Path to .env file")
+	pPath := flag.String("env", "env.json", "Path to .env file")
 	flag.Parse()
 
-	keys, err := parseEnvFile(*pPath)
+	requiredKeys, err := parseEnvFile(*pPath)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	errors := checkEnvVars(keys)
-	if len(errors) != 0 {
-		fmt.Printf("Errors found: %d\n", len(errors))
-		for i, err := range errors {
-			fmt.Printf("%d. %v\n", i+1, err)
+	missingKeys := checkEnvVars(requiredKeys)
+	if len(missingKeys) != 0 {
+		msg := fmt.Sprintf("ERROR: some variables are undefined: %d\n", len(missingKeys))
+		for i, key := range missingKeys {
+			msg = fmt.Sprintf("%s%d. %v\n", msg, i+1, key)
 		}
+		fmt.Println(msg)
 		os.Exit(1)
 	}
 
-	fmt.Printf("All %d env vars found: OK\n", len(keys))
-}
-
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, fmt.Errorf("error opening file: %v", err)
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
+	fmt.Printf("All environment variables found: %d\n", len(requiredKeys))
 }
 
 func parseEnvFile(path string) ([]string, error) {
 	vars := []string{}
-	lines, err := readLines(path)
+	bs, err := os.ReadFile(path)
 	if err != nil {
 		return vars, fmt.Errorf("error reading env file: %v", err)
 	}
 
-	re, err := regexp.Compile(`(\w+)=?`)
+	err = json.Unmarshal(bs, &vars)
 	if err != nil {
-		return vars, fmt.Errorf("error parsing env regex: %v", err)
-	}
-
-	for i, line := range lines {
-		if line == "" {
-			continue
-		}
-
-		lineNumber := i + 1
-
-		if !re.MatchString(line) {
-			return vars, fmt.Errorf("error parsing env, line %d is invalid: '%s'", lineNumber, line)
-		}
-
-		matches := re.FindStringSubmatch(line)
-		minMatches := 2
-		if len(matches) < minMatches {
-			return vars, fmt.Errorf(
-				"error parsing env, line %d has not enough matches: '%s'",
-				lineNumber,
-				line,
-			)
-		}
-
-		varName := matches[1]
-		vars = append(vars, varName)
+		return vars, fmt.Errorf("error pargins env file: %v", err)
 	}
 
 	return vars, nil
 }
 
-func checkEnvVars(keys []string) []error {
-	errors := []error{}
+func checkEnvVars(keys []string) []string {
+	missingKeys := []string{}
 	for _, key := range keys {
 		_, exists := os.LookupEnv(key)
 		if !exists {
-			errors = append(errors, fmt.Errorf("env var '%s' not found", key))
+			missingKeys = append(missingKeys, key)
 		}
 	}
 
-	return errors
+	return missingKeys
 }
